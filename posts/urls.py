4e52a2fe713086ac341d4d0ea10b6eb7a8b1c24f
kanami miyakawa <kedamap@yahoo.co.jp>
2022-02-19 08:48:49 +0000

    fix url names

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:post_id>/', views.detail, name='detail')
+    path('', views.index, name='posts_index'),
+    path('<int:post_id>/', views.detail, name='posts_detail')
 ]
