e890b40ae8677d28bdbfd786e34d23751cbb4817
kanami miyakawa <kedamap@yahoo.co.jp>
2022-02-19 07:13:43 +0000

    create posts_detail

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -7,4 +7,7 @@ def index(request):
     posts = Post.objects.order_by('-pulished')
     return render(request, 'posts/index.html', {'posts': posts})
 
+def detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'posts/detail.html', {'post': post})
 # Create your views here.
