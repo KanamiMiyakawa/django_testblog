6057ea184f61c9ca28b25d3473ef4df4ca94a76f
kanami miyakawa <kedamap@yahoo.co.jp>
2022-02-19 10:21:09 +0000

    change posts:index to generic view

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse
+from django.views import generic
+
 from .models import Post
 
-def index(request):
-    # return HttpResponse("Hello World!")
-    posts = Post.objects.order_by('-pulished')
-    return render(request, 'posts/index.html', {'posts': posts})
+# def index(request):
+#     # return HttpResponse("Hello World!")
+#     posts = Post.objects.order_by('-pulished')
+#     return render(request, 'posts/index.html', {'posts': posts})
+
+class IndexView(generic.ListView):
+    template_name = 'posts/index.html'
+    context_object_name = 'latest_post_list'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-pulished')[:5]
 
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
